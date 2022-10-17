f34c2308b75dc88773049f04aa885dfa102c8d05
ozansann <30535316+ozansann@users.noreply.github.com>
2022-10-17 21:49:55 +0300

    Update AppConstants.java

--- app/src/main/java/com/turkeynews/Constants/AppConstants.java
+++ app/src/main/java/com/turkeynews/Constants/AppConstants.java
@@ -2,6 +2,6 @@ package com.turkeynews.Constants;
 
 public class AppConstants {
     public static final String BASE_URL = "https://newsapi.org/v2/";
-    public static final String API_KEY = "18977dc80ea54c2cba35674a8fc6a550";
+    public static final String API_KEY = "YOUR_API_KEY";
     public static final String[] NEWS_CATEGORIES = {"","science","sports"};
 }
